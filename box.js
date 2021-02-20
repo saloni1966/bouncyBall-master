4efacf56d8986a18555564bb97430aef78161703
saloni1966 <salonisharma1966@gmail.com>
2021-02-20 16:32:35 +0530

    first commit

---
+++ box.js
@@ -0,0 +1,36 @@
+//make box class
+class Box{
+
+    //properties
+    constructor(x,y,width,height){
+
+        var options ={
+            restitution: 0.8,
+            density : 0.3,
+            friction : 0.3
+        }
+    
+       this.body = Bodies.rectangle(x,y,width,height, options);
+       this.width = width;
+       this.height = height;
+       World.add(world,this.body);
+    
+    }
+
+
+    //functions
+    display(){
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        fill("blue");
+        rect(0,0,this.width,this.height);
+        pop();
+
+    }
+
+}
---
+++ sketch.js
@@ -0,0 +1,25 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground,box1;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(200,100,50,90);
+
+  
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    box1.display();
+   
+
+  
+}
